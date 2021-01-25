25634ed8ff478dfde167729f841e368b60dec2b2
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2021-01-25 20:20:51 +0100

    Update fibonacci.py

--- Algorithmic-Toolbox/Week2/fibonacci.py
+++ Algorithmic-Toolbox/Week2/fibonacci.py
@@ -1,13 +1,18 @@
 # Uses python3
+# Task. Given an integer 𝑛, find the 𝑛th Fibonacci number 𝐹𝑛.
+# Input Format. The input consists of a single integer 𝑛.
+# Constraints. 0 ≤ 𝑛 ≤ 45.
+# Output Format. Output 𝐹𝑛.
+
 def calc_fib(n):
-    F = []
-    for i in range(n + 1):
+    F = [] # Initialize for an empty list
+    for i in range(n + 1): # Up to n
         if i == 0:
-            F.append(0)
+            F.append(0) # Base case for recurrence relation
         elif i == 1:
-            F.append(1)
+            F.append(1) # Base case for recurrence relation
         else:
-            F.append(F[i - 1] + F[i - 2])
-    return F[-1]
+            F.append(F[i - 1] + F[i - 2]) # Recurrence relation F[n] = F[n-1] + F[n-2] with F[0] = 0 and F[1] = 1
+    return F[-1] # Return the last element using -1 index
 n = int(input())
 print(calc_fib(n))
