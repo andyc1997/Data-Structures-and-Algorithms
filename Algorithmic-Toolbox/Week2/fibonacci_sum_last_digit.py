047fb81fd0f4731aa7308b9bf8586bfb471d5500
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2021-01-25 22:15:11 +0100

    Update fibonacci_sum_last_digit.py

--- Algorithmic-Toolbox/Week2/fibonacci_sum_last_digit.py
+++ Algorithmic-Toolbox/Week2/fibonacci_sum_last_digit.py
@@ -6,32 +6,16 @@ import sys
 # Output Format. Output the last digit of F[0] + F[1] + ... + F[n].
 
 def get_fibonacci_huge_naive(n, m):
-    # Return F[n] mod m
     if n <= 1:
         return n
-
-    F = []
-    for i in range(n + 1):
-        if i == 0:
-            F.append(0)
-        elif i == 1:
-            F.append(1)
-        else:
-            F.append((F[i - 1] + F[i - 2]) % m)
-        if F[i - 1] == 0 and F[i] == 1 and i > 1:
-            del F[i]
-            del F[i - 1]
-            
-            index = n % len(F)
-            return(F[index])
-            
+    F = [0, 1]
+    for i in range(2, n + 1):
+        F.append((F[i - 1] + F[i - 2]) % m)
+        if F[i - 1] == 0 and F[i] == 1:
+            return(F[n % len(F[:(i - 1)])])            
     return F[-1]
 
-
 def fibonacci_sum_naive(n):
-    # If n = 0 or n = 1 (base cases), it's trivial.
-    if n <= 1:
-        return n
     # The last digit of F[0] + F[1] + ... + F[n] is (F[0] + F[1] + ... + F[n]) mod 10
     # Mathematical induction: F[0] mod 10 = 0 and F[2] (mod 10) - 1 = 0
     # (F[0] + F[1]) mod 10 = 1 mod 10 = 1  and F[3] (mod 10) - 1 = 1
