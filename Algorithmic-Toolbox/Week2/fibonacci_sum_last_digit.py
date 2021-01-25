f717a30cae668e9bed54823d370e165c1deb66bf
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2021-01-25 21:58:37 +0100

    Update fibonacci_sum_last_digit.py

--- Algorithmic-Toolbox/Week2/fibonacci_sum_last_digit.py
+++ Algorithmic-Toolbox/Week2/fibonacci_sum_last_digit.py
@@ -1,7 +1,12 @@
 # Uses python3
 import sys
+# Task. Given an integer n, find the last digit of the sum F[0] + F[1] + · · · + F[n].
+# Input Format. The input consists of a single integer n.
+# Constraints. 0 <= n <= 1014.
+# Output Format. Output the last digit of F[0] + F[1] + ... + F[n].
 
 def get_fibonacci_huge_naive(n, m):
+    # Return F[n] mod m
     if n <= 1:
         return n
 
@@ -24,9 +29,19 @@ def get_fibonacci_huge_naive(n, m):
 
 
 def fibonacci_sum_naive(n):
+    # If n = 0 or n = 1 (base cases), it's trivial.
     if n <= 1:
         return n
+    # The last digit of F[0] + F[1] + ... + F[n] is (F[0] + F[1] + ... + F[n]) mod 10
+    # Mathematical induction: F[0] mod 10 = 0 and F[2] (mod 10) - 1 = 0
+    # (F[0] + F[1]) mod 10 = 1 mod 10 = 1  and F[3] (mod 10) - 1 = 1
+    # Suppose it's true that (F[0] + ... + F[k - 1]) mod 10 = F[k + 1] (mod 10) - 1.
+    # (F[0] + ... + F[k]) mod 10 = (F[k + 1] (mod 10) - 1) + F[k] (mod 10) = (F[k] + F[k + 1]) mod 10 - 1 = F[k + 2] (mod 10) - 1
+    # By M.I., we proved that F[0] + F[1] + ... + F[n] = F[n + 2] (mod 10) - 1,
+    # which can be computed using get_fibonacci_huge_naive(n + 2, 10) - 1
     last_digit = get_fibonacci_huge_naive(n + 2, 10) - 1
+    # Special case: F[n + 2] mod 10 = 0. Then, last digit = -1 which does not make sense.
+    # By basic modular arithmetic, we add back 10 to get 9.
     if last_digit < 0:
         last_digit += 10
     return(last_digit)
