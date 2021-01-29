00a3ac0b97cc6d15d6a7518eb3fc00cb6c47e72a
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2021-01-29 06:20:39 +0100

    Update points_and_segments.py

--- Algorithmic-Toolbox/Week4/points_and_segments.py
+++ Algorithmic-Toolbox/Week4/points_and_segments.py
@@ -37,17 +37,17 @@ def merge_sort(array):
 def modified_binary_search_inf(array, value, l, r): # Time complexity: O(logn)
     # Input: sorted ending points of segments
     # Return: the index of the largest ending points < point
-    m = math.floor((l + r)/2)
-    if value == array[m]:
+    m = math.floor((l + r)/2) # mid point
+    if value == array[m]: # If there is an exact match, do backward linear search
         while value == array[m]:
             m -= 1
-            if m < 0:
+            if m < 0: # Case if given point is the minimum, then no largest ending points => return -1
                 return -1
-        return m
+        return m # Otherwise, return such point
     if l == r:
-        if array[l] < value:
+        if array[l] < value: # If there is no exact match, check if the ending point found is smaller than the given point. If so, return it.
             return l
-        return l - 1
+        return l - 1 # If not, it's possible that 1) array[m] < value < array[m + 1] or 2) value < array[m]. First case => set l := m+1 and r := r. When l == r, it's possible lower bound on a unit left. If original index is already 0, it will return -1.
     elif value < array[m]:
         return modified_binary_search_inf(array, value, l, m)
     else:
@@ -57,16 +57,16 @@ def modified_binary_search_sup(array, value, l, r): # Time complexity: O(logn)
     # Input: sorted starting points of segments
     # Return: the index of the smallest starting points > point
     m = math.floor((l + r)/2)
-    if value == array[m]:
+    if value == array[m]: # If there is an exact match, do forward linear search
         while value == array[m]:
             m += 1
-            if m > len(array) - 1:
+            if m > len(array) - 1: # Case if given point is the maximum, then no smallest starting points => return -1
                 return -1
-        return m
+        return m # Otherwise, return such point
     if l == r:
-        if array[l] > value:
+        if array[l] > value: # If there is no exact match, check if the starting point found is greater than the given point. If so, return it.
             return l
-        return -1
+        return -1 # If not, return not found. As value > array[m] will push l to m + 1 when array[m] < value < array[m + 1], such case must be value > array[len(array) - 1] 
     elif value > array[m]:
         return modified_binary_search_sup(array, value, m + 1, r)
     else:
@@ -75,13 +75,13 @@ def modified_binary_search_sup(array, value, l, r): # Time complexity: O(logn)
 def fast_count_segments(starts, ends, points):
     cnt = list()
     index_begin, index_stop = 0, len(starts) - 1
-    sort_starts = merge_sort(starts)
-    sort_ends = merge_sort(ends)
+    sort_starts = merge_sort(starts) # Sort starting points of all segments
+    sort_ends = merge_sort(ends) # Sort ending points of all segments
     for p in points:
-        p_sup = modified_binary_search_sup(sort_starts, p, index_begin, index_stop) 
-        p_inf = modified_binary_search_inf(sort_ends, p, index_begin, index_stop) 
-        num_segment_above, num_segment_below = (len(starts) - p_sup)*(p_sup > -1), (p_inf + 1)*(p_inf > -1)
-        num = len(starts) - (num_segment_above + num_segment_below)
+        p_sup = modified_binary_search_sup(sort_starts, p, index_begin, index_stop) # For a point p, we find the smallest starting point above p. 
+        p_inf = modified_binary_search_inf(sort_ends, p, index_begin, index_stop)  # For a point p, we also find the largest ending point below p.
+        num_segment_above, num_segment_below = (len(starts) - p_sup)*(p_sup > -1), (p_inf + 1)*(p_inf > -1) # The number of segment above p is total number of starting points - the index of the smallest starting point. The number of segment below p is the index of the largest ending point + 1 (python starts at index 0)
+        num = len(starts) - (num_segment_above + num_segment_below) # Boolean variables are introduced in case if no segment above/below a certain point, set to 0. So, total number of segments covering p = total number of segments - total number of excluded segments.
         cnt.append(num)
     return cnt
 
