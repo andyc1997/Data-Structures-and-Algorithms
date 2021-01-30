754fa46c39cf6a82258f91dd97651614c2b3e19c
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2021-01-30 01:44:31 +0100

    Update lcs2.py

--- Algorithmic-Toolbox/Week5/lcs2.py
+++ Algorithmic-Toolbox/Week5/lcs2.py
@@ -1,12 +1,14 @@
 #Uses python3
-
 import sys
+# Task. Given two sequences A = (a[1], a[2], ... , a[n]]) and B = (b[1], b[2]], ,,, , b[m]), find the length of their longest
+# common subsequence, i.e., the largest non-negative integer p such that there exist indices 1 <= i[1] <
+# i[2] < ... < i[p] <= n and 1 <= j[1] < j[2] < ... < j[p] <= m, such that a[i[1]] = b[j[1]] , ... , a[i[p]] = b[j[p]].
+# Input Format. First line: n. Second line: a[1], a[2], ... , a[n]. Third line: m. Fourth line: b[1], b[2], ... , b[m].
+# Constraints. 1 <= n, m <= 100; −109 < a[i], b[i] < 109.
+# Output Format. Output p.
 
 def edit_distance(s, t):
-    #write your code here
-    # Build zero matrix
     D = [(len(t) + 1) * [0] for i in range(len(s) + 1)]
-    # Initialize variable
     for i in range(1, len(s) + 1):
         D[i][0] = i
     for i in range(1, len(t) + 1):
@@ -16,7 +18,7 @@ def edit_distance(s, t):
             insertion = D[i][j - 1] + 1
             deletion = D[i - 1][j] + 1
             match = D[i - 1][j - 1] 
-            mismatch = D[i - 1][j - 1] + 1
+            mismatch = D[i - 1][j - 1] + 2 # Same as the calculation of edit distance but change the penalty of mismatch to 2
             if s[i - 1] == t[j - 1]:
                 D[i][j] = min(insertion, deletion, match)
             else:
@@ -25,15 +27,12 @@ def edit_distance(s, t):
     count, common = 0, 0
     while (i != 0) | (j != 0):
         if (i > 0) & (D[i][j] == D[i - 1][j] + 1):
-            #print([s[i - 1], '-'])
             count += 1
             i -= 1
         elif (j > 0) & (D[i][j] == D[i][j - 1] + 1):
             count += 1
-            #print(['-', t[j - 1]])
             j -= 1
         else:
-            #print([s[i - 1], t[j - 1]])
             if s[i - 1] != t[j - 1]:
                 count += 1
             else:
@@ -43,13 +42,11 @@ def edit_distance(s, t):
     return common
 
 def lcs2(a, b):
-    #write your code here
     return edit_distance(a, b) 
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
-
     n = data[0]
     data = data[1:]
     a = data[:n]
