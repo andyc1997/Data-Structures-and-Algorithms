79de55c37efc5ba7d19d79e5d9c67148f2fccd00
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2021-02-01 05:24:32 +0100

    Update build_heap.py

--- Data-Structures/Week3/build_heap.py
+++ Data-Structures/Week3/build_heap.py
@@ -1,62 +1,49 @@
 # python3
+# Input Format. The first line of the input contains single integer n. The next line contains n space-separated
+# integers a[i].
+# Output Format. The first line of the output should contain single integer m — the total number of swaps.
+# m must satisfy conditions 0 <= m <= 4*n. The next m lines should contain the swap operations used
+# to convert the array a into a heap. Each swap is described by a pair of integers i, j — the 0-based
+# indices of the elements to be swapped. After applying all the swaps in the specified order the array
+# must become a heap, that is, for each i where 0 <= i <= n − 1 the following conditions must be true:
+# 1. If 2*i + 1 <= n − 1, then a[i] < a[2*i + 1].
+# 2. If 2*i + 2 <= n − 1, then a[i] < a[2*i + 2].
+# Note that all the elements of the input array are distinct. Note that any sequence of swaps that has
+# length at most 4*n and after which your initial array becomes a correct heap will be graded as correct.
 
-
-def build_heap(data):
-    """Build a heap from ``data`` inplace.
-
-    Returns a sequence of swaps performed by the algorithm.
-    """
-    # The following naive implementation just sorts the given sequence
-    # using selection sort algorithm and saves the resulting sequence
-    # of swaps. This turns the given array into a heap, but in the worst
-    # case gives a quadratic number of swaps.
-    #
-    # TODO: replace by a more efficient implementation
-    swaps = []
-    for i in range(len(data)):
-        for j in range(i + 1, len(data)):
-            if data[i] > data[j]:
-                swaps.append((i, j))
-                data[i], data[j] = data[j], data[i]
-    return swaps
-
-class Heap():
+class Heap(): # A binary min-heap
     def __init__(self, arr):
-        self.arr = arr
-        self.swap = []
+        self.arr = arr # Input array
+        self.swap = [] # Store swap operations
         
-    def LeftChild(i):
+    def LeftChild(i): # return the index of left child
         return 2 * i + 1
     
-    def RightChild(i):
+    def RightChild(i): # return the index of right child
         return 2 * i + 2
     
-    def Parent(i):
+    def Parent(i): # return the index of parent
         return (i - 1) // 2
 
     def SiftDown(self, i):
         minIndex = i
-        # Left
-        l = Heap.LeftChild(i)
+        l = Heap.LeftChild(i) # Compare if left child has a key less than that of input node.
         if l < len(self.arr):
             if self.arr[l] < self.arr[minIndex]:
                 minIndex = l
-        # Right
         r = Heap.RightChild(i)
         if r < len(self.arr):
-            if self.arr[r] < self.arr[minIndex]:
+            if self.arr[r] < self.arr[minIndex]: # Compare if left child has a key less than that of input node or left child.
                 minIndex = r
-        # Update
         if i != minIndex:
-            self.swap.append((i, minIndex))
-            self.arr[i], self.arr[minIndex] = self.arr[minIndex], self.arr[i]
-            Heap.SiftDown(self, minIndex)
+            self.swap.append((i, minIndex)) # Record swapping operation
+            self.arr[i], self.arr[minIndex] = self.arr[minIndex], self.arr[i] # Swap input node with the children with the minimal key
+            Heap.SiftDown(self, minIndex) # Do it recursively until no more swapping can be done
 
 def BuildHeap(arr):
     size = len(arr)
     heap = Heap(arr)
-    
-    for i in reversed(range(0, size // 2)):
+    for i in reversed(range(0, size // 2)): # The last n / 2 are leaves. We start at the second last level and push the minimum key up for each subtree
         heap.SiftDown(i)
     return heap.swap
 
@@ -64,18 +51,12 @@ def main():
     n = int(input())
     data = list(map(int, input().split()))
     assert len(data) == n
-    
+  
     swap = BuildHeap(data)
     print(len(swap))
     if len(swap) > 0:
         for i, j in swap:
             print(i, j)
-    #swaps = BuildHeap(data)
-
-    #print(len(swaps))
-    #for i, j in swaps:
-    #    print(i, j)
-
 
 if __name__ == "__main__":
     main()
