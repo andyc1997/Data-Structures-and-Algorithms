cc4537b4c9bf62f1d9ce79d7c1e5c4c254fc2f48
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2021-02-02 19:02:03 +0100

    Update hash_chains.py

--- Data-Structures/Week4/hash_chains.py
+++ Data-Structures/Week4/hash_chains.py
@@ -1,7 +1,17 @@
 # python3
+# Task. In this task your goal is to implement a hash table with lists chaining. You are already given the
+# number of buckets m and the hash function. It is a polynomial hash function. 
+# Your program should support the following kinds of queries:
+# 1. add string — insert string into the table. If there is already such string in the hash table, then just ignore the query.
+# 2. del string — remove string from the table. If there is no such string in the hash table, then just ignore the query.
+# 3. find string — output “yes" or “no" (without quotes) depending on whether the table contains string or not.
+# 4. check i — output the content of the i-th list in the table. Use spaces to separate the elements of the list. If i-th list is empty, output a blank line.
+# Input Format. There is a single integer m in the first line — the number of buckets you should have. The next line contains the number of queries N. 
+# It’s followed by N lines, each of them contains one query in the format described above.
+# Output Format. Print the result of each of the find and check queries, one result per line, in the same
+# order as these queries are given in the input.
 
 class Query:
-
     def __init__(self, query):
         self.type = query[0]
         if self.type == 'check':
@@ -9,16 +19,13 @@ class Query:
         else:
             self.s = query[1]
 
-
 class QueryProcessor:
     _multiplier = 263
     _prime = 1000000007
 
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
-        # store all strings in one list
         self.elems = dict()
-        #self.elems = []
 
     def _hash_func(self, s):
         ans = 0
@@ -37,9 +44,6 @@ class QueryProcessor:
 
     def process_query(self, query):
         if query.type == "check":
-            # use reverse order, because we append strings to the end
-            #self.write_chain(cur for cur in reversed(self.elems)
-            #            if self._hash_func(cur) == query.ind)
             if query.ind in self.elems:
                 self.write_chain(reversed(self.elems[query.ind]))
             else:
@@ -50,10 +54,7 @@ class QueryProcessor:
             if hashed_string in self.elems:
                 if query.s in self.elems[hashed_string]:
                     ind = self.elems[hashed_string].index(query.s)
-#            try:
-#                ind = self.elems.index(query.s)
-#            except ValueError:
-#                ind = -1
+
             if query.type == 'find':
                 self.write_search_result(ind != -1)
             elif query.type == 'add':
@@ -62,11 +63,9 @@ class QueryProcessor:
                         self.elems[hashed_string].append(query.s)
                     else:
                         self.elems.update({hashed_string: [query.s]})
-                    #self.elems.append(query.s)
             else:
                 if ind != -1:
                     self.elems[hashed_string].pop(ind)
-                    #self.elems.pop(ind)
 
     def process_queries(self):
         n = int(input())
