4ab090d8ec40c7cc760dbec47113783f97e1f133
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2021-02-05 09:44:04 +0100

    Update rope.py

--- Data-Structures/Week6/rope.py
+++ Data-Structures/Week6/rope.py
@@ -1,14 +1,23 @@
 # python3
 import sys
+# Task. You are given a string S and you have to process n queries. Each query is described by three integers
+# i, j, k and means to cut substring S[i...j] (i and j are 0-based) from the string and then insert it after the
+# k-th symbol of the remaining string (if the symbols are numbered from 1). If k = 0, S[i...j] is inserted
+# in the beginning. See the examples for further clarification.
+# Input Format. The first line contains the initial string S.
+# The second line contains the number of queries q.
+# Next q lines contain triples of integers i, j, k.
+# Output Format. Output the string after all q queries.
+
 class Rope:
-    def __init__(self, s):
+    def __init__(self, s): # Input string
         self.s = s
         
-    def result(self):
+    def result(self): # Return results
         return self.s
-    def process(self, i, j, k):
-        # Write your code here
-        substr = self.s[i:j + 1]
+    
+    def process(self, i, j, k): # A naive solution
+        substr = self.s[i:j + 1] # Substring defined in the problem
         self.s = self.s[:i] + self.s[j + 1:]
         if k == 0:
             self.s = substr + self.s
@@ -22,11 +31,4 @@ for _ in range(q):
     rope.process(i, j, k)
 print(rope.result())
 
-#%%
-# rope = Rope('hlelowrold'.strip())
-# data = ['1 1 2', '6 6 7']
-# q = int('2')
-# for s in data:
-#     i, j, k = map(int, s.strip().split())
-#     rope.process(i, j, k)
-# print(rope.result())
+# Remark: I haven't figured out how to implement a splay tree to solve this problem. So, I use naive solution at the moment
