b6fa8dcfa9ea7f62dfed39333cef8f5898f394ca
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2021-02-07 20:08:38 +0100

    Update negative_cycle.py

--- Algorithms-on-Graphs/Week4/negative_cycle.py
+++ Algorithms-on-Graphs/Week4/negative_cycle.py
@@ -1,30 +1,29 @@
 #Uses python3
-
 import sys
-
+# Task. Given an directed graph with possibly negative edge weights and with n vertices and m edges, check
+# whether it contains a cycle of negative weight.
+# Input Format. A graph is given in the standard format.
+# Output Format. Output 1 if the graph contains a cycle of negative weight and 0 otherwise.
 
 def negative_cycle(adj, cost):
-    #write your code here
     n = len(adj)
-    inf = (10 ** 3) * n + 1
+    inf = float('inf')
     dist = [inf] * n
-    
-    for j in range(n): # Loop |V| - 1 times
-        for u in range(n):
+    # a standard implementation of Bellman-Ford algorithm in O(|V||E|) time
+    for j in range(n): # Loop |V| times
+        for u in range(n): # Run all edges
             if len(adj[u]) != 0:
-                for i in range(len(adj[u])): # Loop |E| times
+                for i in range(len(adj[u])):
                     v = adj[u][i]
-                    if dist[v] > dist[u] + cost[u][i]:
+                    if dist[v] > dist[u] + cost[u][i]: # Edge relaxation
                         dist[v] = dist[u] + cost[u][i]
-                        if (j == n - 1):
+                        if (j == n - 1): # If there is edge relaxation in the |V|-th iteration, it imples a negative cycle
                             return 1
     return 0
-
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
-    #data = [4, 4, 1, 2, -5, 4, 1, 2, 2, 3, 2, 3, 1, 1]
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
