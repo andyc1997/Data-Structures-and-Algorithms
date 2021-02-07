118281f99a3469bb6b615036d84aa99cd0d00e41
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2021-02-07 10:59:55 +0100

    Update bfs.py

--- Algorithms-on-Graphs/Week3/bfs.py
+++ Algorithms-on-Graphs/Week3/bfs.py
@@ -1,36 +1,34 @@
 #Uses python3
-
 import sys
 import queue
+# Task. Given an undirected graph with n vertices and m edges and two vertices u and v, compute the length
+# of a shortest path between u and v (that is, the minimum number of edges in a path from u to v).
+# Input Format. A graph is given in the standard format. The next line contains two vertices u and v.
+# Output Format. Output the minimum number of edges in a path from u to v, or −1 if there is no path.
 
 def distance(adj, s, t):
-    #write your code here
-    n = len(adj) # Number of vertices
-    inf = n + 1 # Distance between the origin and isolated points
-    
-    dist = [inf] * n # An initialized array containing distance of each vertex
-    dist[s] = 0
-    
-    q_vertex = queue.Queue()
+    n = len(adj) # number of vertices
+    inf = n + 1 # distance between the origin and isolated points
+    dist = [inf] * n # an initialized array containing distance of each vertex
+    dist[s] = 0 # distance to itself is 0 
+    q_vertex = queue.Queue() # a FIFO queue to store nodes in breadth first search
     q_vertex.put(s)
     
     while not q_vertex.empty():
         u = q_vertex.get()
-        for v in adj[u]:
-            if dist[v] == inf:
+        for v in adj[u]: # search the neighborhood of u
+            if dist[v] == inf: # if it's not visited before, enqueue it and d(v, s) = d(u, s) + 1 because each edge has the same weighting
                 q_vertex.put(v)
                 dist[v] = dist[u] + 1
     
-    if dist[t] != inf:
-        return dist[t]
+    if dist[t] != inf: # if dist[t] != inf, t is reachable from s
+        return dist[t] # output the number edges
     else:
-        return -1
+        return -1 # otherwise, it is unreachable from s, output -1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
-    #data = [4, 4, 1, 2, 4, 1, 2, 3, 3, 1, 2, 4]
-    #data = [5, 4, 5, 2, 1, 3, 3, 4, 1, 4, 3, 5]
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
