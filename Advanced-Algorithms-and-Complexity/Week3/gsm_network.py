0785131f6755a9e183810cb1283a55e5c856376e
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2021-02-11 20:32:29 +0100

    Update gsm_network.py

--- Advanced-Algorithms-and-Complexity/Week3/gsm_network.py
+++ Advanced-Algorithms-and-Complexity/Week3/gsm_network.py
@@ -1,24 +1,10 @@
 # python3
 # -*- coding: utf-8 -*-
 import itertools
+# This is equivalent to a classical graph coloring problem: in other words, you are given a graph, and
+# you need to color its vertices into 3 different colors, so that any two vertices connected by an edge
+# need to be of different colors.
 
-#%% Default mapping 
-n, m = map(int, input().split())
-edges = [ list(map(int, input().split())) for i in range(m) ]
-
-#%% Test case 1
-# n, m = [3, 3]
-# edges = [[1, 2], [2, 3], [1, 3]]
-
-#%% Test case 2
-# n, m = [4, 6]
-# edges = [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
-
-#%% Test case 3
-# n, m = [5, 7]
-# edges = [[1, 2], [1, 3], [1, 4], [1, 5], [2, 3], [3, 4], [4, 5]]
-
-#%% Main program
 vertex = range(1, n + 1)
 color = range(1, 4)
 naming_scheme = [[v + (c - 1)*max(vertex) for v in vertex] for c in color]
@@ -43,10 +29,9 @@ def create_adj_list(edges, n):
     adj_list = [set() for i in range(n)]
     for e in edges:
         adj_list[e[0] - 1].add(e[1] - 1)
-        # adj_list[e[1] - 1].add(e[0] - 1)
     return adj_list
 
-def printEquisatisfiableSatFormula():
+def sat_formula():
     # Intialize variables
     adj_list = create_adj_list(edges, n)
     total_clause = empty_clause()
@@ -61,10 +46,12 @@ def printEquisatisfiableSatFormula():
         clause = empty_clause() # Temporary clauses
         for v in vertex:
             literal = []
+            # At least one vertex is colored
             for c in color:
                 var = varnum(v, c)
                 literal.append(var)
             clause.append(literal)
+            # Exactly one vertex is colored
             for c in color:
                 literal_temp = literal.copy()
                 literal_temp.pop(c - 1)
@@ -86,11 +73,4 @@ def printEquisatisfiableSatFormula():
     print('{} {}\n'.format(len(total_clause), 3000))
     print_clause(total_clause)
     
-#%% Write output to .txt file for minisat, uncomment if necessary
-    # with open('tmp.txt', 'w') as f:
-    #     f.write('p cnf {} {}\n'.format(120000, len(total_clause)))
-    #     for c in total_clause:
-    #         c.append(0)
-    #         f.write(" ".join(map(str, c)) + '\n')
-    
-printEquisatisfiableSatFormula()
+sat_formula()
