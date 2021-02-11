062ea1e28801ec519a5f33ffd5a4fa3e84a831bd
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2021-02-11 17:42:02 +0100

    Update and rename energy_values (submit).py to energy_values.py

--- Advanced-Algorithms-and-Complexity/Week2/energy_values.py
+++ Advanced-Algorithms-and-Complexity/Week2/energy_values.py
@@ -1,97 +1,71 @@
 # python3
 EPS = 1e-6
 PRECISION = 20
+# Task. You’re looking into a restaurant menu which shows for each dish the list of ingredients with amounts
+# and the estimated total energy value in calories. You would like to find out the energy values of
+# individual ingredients (then you will be able to estimate the total energy values of your favorite dishes).
+# Input Format. The first line of the input contains an integer n — the number of dishes in the menu, and
+# it happens so that the number of different ingredients is the same. Each of the next n lines contains
+# description a[1], a[2], . . . , a[n], E of a single menu item. a[i] is the amount of i-th ingredient in the dish, and
+# E is the estimated total energy value of the dish. If the ingredient is not used in the dish, the amount
+# will be specified as a[i] = 0; beware that although the amount of any ingredient in any real
+# menu would be positive, we will test that your algorithm works even for negative amounts a[i] < 0.
+# Output Format. Output n real numbers — for each ingredient, what is its energy value. These numbers
+# can be non-integer, so output them with at least 3 digits after the decimal point.
 
-
-# In[2]:
-
-
-class Equation:
+class Equation: # an equation object with Ax = b
     def __init__(self, a, b):
         self.a = a
         self.b = b
-
-
-# In[3]:
-
 
 class Position:
     def __init__(self, column, row):
         self.column = column
         self.row = row
 
-
-# In[4]:
-
-
-def ReadEquation():
+def ReadEquation(): # read input data
     size = int(input())
     a = []
     b = []
     for row in range(size):
         line = list(map(float, input().split()))
-        a.append(line[:size])
-        b.append(line[size])
+        a.append(line[:size]) # First 'size' elements are coefficients
+        b.append(line[size]) # the last element is constraint on total
     return Equation(a, b)
-
-# For testing purposes:
-def ReadEquationTest():
-    #data = [[1, 0, 0, 0, 1], [0, 1, 0, 0, 5], [0, 0, 1, 0, 4], [0, 0, 0, 1, 3]]
-    #data = [[1, 1, 3], [2, 3, 7]]
-    #data = [[5, -5, -1], [-1, -2, -1]]
-    data = [[1, 0, 0, 0, 1], [0, 0, 0, 1, 3], [0, 1, 0, 0, 5], [0, 0, 1, 0, 8]]
-    size = len(data)
-    a = []
-    b = []
-    for row in range(size):
-        line = list(map(float, data[row]))
-        a.append(line[:size])
-        b.append(line[size])
-    return Equation(a, b)
-
-
-# In[5]:
-
 
 def SelectPivotElement(a, b, used_rows, used_columns):
-    # This algorithm selects the first free element.
-    # You'll need to improve it to pass the problem.
-    pivot_element = Position(0, 0)
-    while used_rows[pivot_element.row]:
+    # Search for the pivot element from the left-top corner
+    # Indices of previous pivot elements are recorded in used_rows and used_columns
+    pivot_element = Position(0, 0) # the first pivot element is the first element at the first row
+    while used_rows[pivot_element.row]: # long as pivot_element.row has been used before in Gaussian elimination, go to next row
         pivot_element.row += 1
-    while used_columns[pivot_element.column]:
+    while used_columns[pivot_element.column]: # long as pivot_element.column has been used before in Gaussian elimination, go to next column
         pivot_element.column += 1
-        
+    # Case 1: pivot element = 0
     max_row = pivot_element.row
-    if a[pivot_element.row][pivot_element.column] == 0:
+    if a[pivot_element.row][pivot_element.column] == 0: # If the pivot element is singular, we need to search for the largest element in row below pivot element within the same column
         for below_row in range(pivot_element.row + 1, len(a)):
-            if a[below_row][pivot_element.column] > a[max_row][pivot_element.column]:
+            if a[below_row][pivot_element.column] > a[max_row][pivot_element.column]: # If the element > previous found largest element, the row as max_row
                 max_row = below_row
+        # After the searching process, do swapping for both coefficient matrix A, and constant vector b
         a[pivot_element.row], a[max_row] = a[max_row], a[pivot_element.row]
         b[pivot_element.row], b[max_row] = b[max_row], b[pivot_element.row]
+    # Case 2: pivot element != 0
+    # the if condition will not be activated and return the found pivot element immediately in this case, no need for searching and swapping
     return pivot_element
 
-
-# In[6]:
-
-
 def SwapLines(a, b, used_rows, pivot_element):
+    # If pivot element is at the diagonal, no swapping 
+    # If the pivot element is found below the diagonal do swapping
     a[pivot_element.column], a[pivot_element.row] = a[pivot_element.row], a[pivot_element.column]
     b[pivot_element.column], b[pivot_element.row] = b[pivot_element.row], b[pivot_element.column]
     used_rows[pivot_element.column], used_rows[pivot_element.row] = used_rows[pivot_element.row], used_rows[pivot_element.column]
     pivot_element.row = pivot_element.column;
 
-
-# In[7]:
-
-
 def ProcessPivotElement(a, b, pivot_element):
-    # Write your code here
-    # Scale elements in pivot row with pivot element = 1
     scaling_factor = a[pivot_element.row][pivot_element.column]
     a[pivot_element.row] = list(map(lambda x: x / scaling_factor, a[pivot_element.row]))
     b[pivot_element.row] /= scaling_factor
-    # All zeros below pivot element
     size = len(a)
     if pivot_element.row + 1 < size:
         for non_pivot in range(pivot_element.row + 1, size):
@@ -99,7 +73,6 @@ def ProcessPivotElement(a, b, pivot_element):
             for column in range(pivot_element.column, len(a[0])):
                 a[non_pivot][column] -= multiple * a[pivot_element.row][column]
             b[non_pivot] -= multiple * b[pivot_element.row]
-    # All zeros above pivot element
     if pivot_element.row > 0:
         for above_pivot in range(0, pivot_element.row):
             multiple = a[above_pivot][pivot_element.column]
@@ -108,17 +81,9 @@ def ProcessPivotElement(a, b, pivot_element):
             b[above_pivot] -= multiple * b[pivot_element.row]
     pass
 
-
-# In[8]:
-
-
 def MarkPivotElementUsed(pivot_element, used_rows, used_columns):
     used_rows[pivot_element.row] = True
     used_columns[pivot_element.column] = True
-
-
-# In[9]:
-
 
 def SolveEquation(equation):
     a = equation.a
@@ -128,24 +93,16 @@ def SolveEquation(equation):
     used_columns = [False] * size
     used_rows = [False] * size
     for step in range(size):
-        pivot_element = SelectPivotElement(a, b, used_rows, used_columns)
+        pivot_element = SelectPivotElement(a, b, used_rows, used_columns) # select the pivot element of each row
         SwapLines(a, b, used_rows, pivot_element)
         ProcessPivotElement(a, b, pivot_element)
         MarkPivotElementUsed(pivot_element, used_rows, used_columns)
     return b
 
-
-# In[10]:
-
-
 def PrintColumn(column):
     size = len(column)
     for row in range(size):
         print("%.20lf" % column[row])
-
-
-# In[11]:
-
 
 if __name__ == "__main__":
     equation = ReadEquation()
