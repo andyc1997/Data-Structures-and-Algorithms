273a8ce04f973f9b610ad8f3872b00f26c4d2ee3
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2021-06-25 22:39:34 +0200

    Update trie.py

--- Algorithms-on-Strings/Week1/trie.py
+++ Algorithms-on-Strings/Week1/trie.py
@@ -2,15 +2,23 @@
 import sys
 
 def build_trie(patterns):
+    # Initialize the root with index 0
     tree = dict({0: dict()})
     node_idx = 0
-    # write your code here
+    
+    # Loop each string in patterns
     for pattern in patterns:
-        currentNode = 0
+        currentNode = 0 # Start from the root
+        
+        # Loop each character in string
         for i in range(len(pattern)):
-            currentSymbol = pattern[i]
+            currentSymbol = pattern[i] # currentSymbol = the current character
+            
+            # If the current character is found in the trie, go to its child node
             if currentSymbol in tree[currentNode]:
                 currentNode = tree[currentNode][currentSymbol]
+            
+            # Otherwise, a new node should be created and go to the new node
             else:
                 node_idx += 1
                 tree.update({node_idx: dict()})
